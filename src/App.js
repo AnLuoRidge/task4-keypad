c84a48e84c9ee8a93c5af00c84ef8dd1f7f9ccbe
Steven Ding <anluoridge@gmail.com>
2019-06-18 22:40:07 +1000

    Create the keypad with map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Display from './components/Display';
 import Button from './components/Button';
 
 function App() {
+  var [displayNum, setDisplayNum] = useState("0");
+  let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  let keypad = numbers.map((number) =>
+    <Button value={number} onClick={() => setDisplayNum(displayNum += number)}/>
+  );
+
   return (
     <div className="App">
-      <Display value="0" />
-
-      <div className="Button-wrapper">
-        <Button value="1" />
-      </div>
+      <Display value={displayNum} />
+      <div className="Button-wrapper">{keypad}</div>
     </div>
   );
 }
