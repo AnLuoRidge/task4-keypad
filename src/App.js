990ba30fc5f28b0aa21de3d7ee4652805b2e5e0f
Steven Ding <anluoridge@gmail.com>
2019-06-19 11:48:21 +1000

    Implement length limit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import Display from './components/Display';
 import Button from './components/Button';
 
 function App() {
-  var [displayNum, setDisplayNum] = useState("0");
-  let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  let keypad = numbers.map((number) =>
-    <Button value={number} onClick={() => setDisplayNum(displayNum += number)}/>
+  var [displayNum, setDisplayNum] = useState("1234567890");
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const keypad = numbers.map((number) =>
+    <Button value={number} onClick={() => updateDisplay(number)} />
   );
+  const updateDisplay = (number) => {
+    displayNum += number;
+    if (displayNum.length > 11) {
+      displayNum = displayNum.substr(1, 11);
+    }
+    setDisplayNum(displayNum)
+  };
+
+
 
   return (
     <div className="App">
